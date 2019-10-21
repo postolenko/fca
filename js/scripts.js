973de1bad7db44c5d1a7f6edd16ce58ca6daa262
postolenko <denis.postolenko@gmail.com>
2019-10-21 20:19:19 +0300

    FCA

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+
+
+});
